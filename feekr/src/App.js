f3dcca77eaf774ba0718aa62a5c3ecbd5d0fed1c
czhv5 <1321857934@qq.com>
2020-06-19 22:14:53 +0800

    主页

--- feekr/src/App.js
+++ feekr/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from './pages/Home';
+import 'antd/dist/antd.css';
 function App() {
   return (
     <div>
 
       <Switch>
         <Route path="/home" component={Home} />
-        <Redirect from="/" to="/home" exact />
         <Route path="/notfound" component={() => <div>404</div>} />
+        <Redirect from="/" to="/home" exact />
+        <Redirect to="/notfound" />
       </Switch>
 
     </div>
